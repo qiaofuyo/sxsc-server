2662f01d4ae5360a172f5750ad1e86eb8845756c
qiaofuyo <1103351140@qq.com>
2020-02-07 00:56:58 +0800

    '首页完成'

--- db/models.js
+++ db/models.js
@@ -1,33 +1,42 @@
-const conn=require('./db.js')
-const mongoose=require('mongoose')
+const conn = require('./db.js')
+const mongoose = require('mongoose')
 
 // 创建Schema
 
 // 首页轮播图
-let homeSlideSchema=new mongoose.Schema({
-	imgurl:String,
-	detail:String
+let homeSlideSchema = new mongoose.Schema({
+	_id: Object,
+	name: String,
+	image_url: String,
+	goto: String,
 })
-// var user=new homeSlideModel({  // 在集合中定义条文档
-// 	imgurl:'http://www.baidu.com/',
-// 	detail:87,
-// 	logindate:new Date()
-// })
-// user.save((err,res)=>{
-// 	if(err) return console.log(err);
-// 	// 打印文档
-// 	homeSlideModel.find((err,data)=>{
-// 		console.log(data);
-// 	})
-// })
-// 首页导航
-let homeNavSchema=new mongoose.Schema({
-	iconurl:String,
-	icontitle:String
+
+// 首页中间导航
+let homeNavSchema = new mongoose.Schema({
+	_id: Object,
+	image_url: String,
+	name: String
+})
+
+// 首页商品列表
+let homeShopListSchema = new mongoose.Schema({
+	_id: String,
+	name: String,
+	describe: String,
+	image_url: String,
+	price: String,
+	origin_price: String,
+	sold: Number,
+	inventory: Number
 })
 
 // 生成Model
-const homeSlideModel=mongoose.model('homeslides',homeSlideSchema)
-const homeNavModel=mongoose.model('homenavs',homeNavSchema)
+const homeSlideModel = mongoose.model('homeslides', homeSlideSchema)
+const homeNavModel = mongoose.model('homenavs', homeNavSchema)
+const homeShopListModel = mongoose.model('homeshoplists', homeShopListSchema)
 
-module.exports ={homeSlideModel,homeNavModel}
+module.exports = {
+	homeSlideModel,
+	homeNavModel,
+	homeShopListModel
+}
--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,22 @@
 const express = require('express');
-// 使用Router模块
 const router = express.Router();
-// // 导入连接数据库
-// const conn=require('../db/db.js')
-// 导入 Model
-const Models = require('../db/models.js')
 
-/* 应用服务器默认首页 */
-router.get('/', function(req, res, next) { // next: 继续执行
+const {
+	homeSlideModel,
+	homeNavModel,
+	homeShopListModel
+} = require('../db/models.js')
+
+router.get('/', function(req, res, next) {
 	res.render('index', {
 		title: 'Express'
 	});
 });
 
-/*
-	服务器 => 后台管理系统,应用服务器(数据分发)
-	这里仅搭建应用服务器的接口部分,不涉及后端操作,全部是数据库操作
-*/
-
 // 响应首页——轮播图图片
 router.get('/api/homeslide', (req, res) => {
-	// 1. 这是通过请求json文件代替数据库,尚未使用数据库
-	// const data=require('../data/homeSlide.json')
-	// console.log(data)
-	// res.json({status_code:200,message:data})  // status_code: 响应的状态码, message: 响应的数据		imgurl:图片地址	detail:跳转页面
-	// 2. 这是请求数据库
 	let sql = {}
-	Models.homeSlideModel.find(sql, (err, docs) => {
+	homeSlideModel.find(sql, (err, docs) => {
 		if (err) {
 			res.json({
 				status_code: 0,
@@ -50,12 +40,8 @@ router.get('/api/homeslide', (req, res) => {
 
 // 响应首页——导航图片
 router.get('/api/homenav', (req, res) => {
-	// 1. 这是通过请求json文件代替数据库,尚未使用数据库
-	// const data=require('../data/homeNav.json')
-	// res.json({success_code:200,message:data})
-	// 2. 这是请求数据库
 	let sql = {}
-	Models.homeNavModel.find(sql, (err, docs) => {
+	homeNavModel.find(sql, (err, docs) => {
 		if (err) {
 			res.json({
 				status_code: 0,
@@ -78,11 +64,28 @@ router.get('/api/homenav', (req, res) => {
 })
 // 响应首页——商品列表图片
 router.get('/api/homeshoplist', (req, res) => {
+	let sql = {}
 	setTimeout(function() { // 为什么要延时3毫秒
-		const data = require('../data/homeShopList.json')
-		res.json({
-			success_code: 200,
-			message: data
+		let sql = {}
+		homeShopListModel.find(sql, (err, docs) => {
+			if (err) {
+				res.json({
+					status_code: 0,
+					message: '查询语句错误!!!'
+				})
+			} else if (docs.length == 0) {
+				res.json({
+					status_code: 1,
+					message: [{
+						'error': '数据库中没有数据!!!'
+					}]
+				})
+			} else {
+				res.json({
+					status_code: 200,
+					message: docs
+				})
+			}
 		})
 	}, 300)
 })
